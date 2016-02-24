aa2881822f1213d4b0ba655eeede7f32cccaf0c4
Ben Ebel <bge1234@gmail.com>
2016-02-24 14:53:19 -0700

    minor tweaks

--- public/js/controllers/main_controller.js
+++ public/js/controllers/main_controller.js
@@ -1,6 +1,6 @@
 app.controller('MainController', function ($scope, $http) {
   $scope.searchByTitle = function() {
-    $http.get("http://www.omdbapi.com/?s=" + $scope.searchterm).then(function(results){
+    $http.get("https://www.omdbapi.com/?s=" + $scope.searchterm).then(function(results){
       $scope.searchresults = results.data;
       $scope.searchterm = null;
     });
--- public/js/controllers/show_controller.js
+++ public/js/controllers/show_controller.js
@@ -1,5 +1,5 @@
 app.controller('ShowController', function ($scope, $http, $routeParams) {
-  $http.get("http://www.omdbapi.com/?i=" + $routeParams.id).then(function(results){
+  $http.get("https://www.omdbapi.com/?i=" + $routeParams.id).then(function(results){
     $scope.movieinfo = results.data;
   });
 });
